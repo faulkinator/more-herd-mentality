f9d896328928a4c7a3f34451df487ec062054f95
aspiramedia <aspiramedia@gmail.com>
2024-01-01 15:48:44 +0000

    INitial commit - basic concept in place.

---
+++ README.md
@@ -0,0 +1,7 @@
+# More Herd Mentality
+
+A simple app for suggesting questions when playing the Herd Mentality board game. Ideal for when the included questions are exhausted by your group.
+
+Also applies to any party game where simple questions are required.
+
+*I am not associated with Herd Mentality or Big Potato Games.*
---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript code to fetch and display a random string from data.json
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Fetch the JSON data
+    fetch('questions.json')
+        .then(response => response.json())
+        .then(data => {
+            const questions = data.questions;
+            const randomIndex = Math.floor(Math.random() * questions.length);
+            const randomQuestionElement = document.getElementById('randomQuestion');
+            randomQuestionElement.textContent = questions[randomIndex];
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});

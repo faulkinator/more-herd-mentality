86a77bc7d34de9b175c3bdd9928064a2a8aa9e72
faulkinator <126863532+faulkinator@users.noreply.github.com>
2024-01-13 16:02:17 +0000

    Next question button added and JSON fetched only once
    To avoid needing to refresh the page, a button has been added to refresh the question.
    
    The questions JSON file is now only fetched once at the start of each session, not on each question.

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Display a random question
+    function refreshQuestion(questions) {
+        const randomIndex = Math.floor(Math.random() * questions.length);
+
+        // Show a random question to the user 
+        const randomQuestion = document.getElementById('randomQuestion');
+        randomQuestion.textContent = `${questions[randomIndex]}`;
+
+        // Show the question number and total to the user
+        const randomQuestionIndex = document.getElementById('randomQuestionIndex');
+        randomQuestionIndex.textContent = `Question ${randomIndex + 1} of ${questions.length}`;
+    }
+
+    // Fetch questions on load
     fetch('questions.json')
         .then(response => response.json())
         .then(data => {
             const questions = data.questions;
-            const randomIndex = Math.floor(Math.random() * questions.length);
-            
-            // Showing a random question to the user 
-            const randomQuestion = document.getElementById('randomQuestion');
-            randomQuestion.textContent = `${questions[randomIndex]}`;
-            
-            // Showing the question number and total to the user
-            const randomQuestionIndex = document.getElementById('randomQuestionIndex');
-            randomQuestionIndex.textContent = `Question ${randomIndex + 1} of ${questions.length}`;
+
+            // Initial question load
+            refreshQuestion(questions);
+
+            // Adding click event listener to the refresh button
+            const refreshButton = document.getElementById('refreshQuestion');
+            refreshButton.addEventListener('click', function () {
+                refreshQuestion(questions);
+            });
         })
         .catch(error => console.error('Error fetching data:', error));
 });
